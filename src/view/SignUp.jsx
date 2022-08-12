320f8e962d2323313c5c7e074ce53caa35ac3eec
somfist <qkrruddjs317@naver.com>
2022-08-12 18:52:07 +0900

    initial

---
+++ src/view/SignUp.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const SignUp = () => {
+    return (
+        <InputArea>
+        </InputArea>
+    )
+}
+
+const InputArea = styled.div`
+    width: 300px;
+    height: 300px;
+    border: 1px solid rgb(255,204,204);
+`
+
+export default SignUp
