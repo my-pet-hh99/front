ed8877af7159d169981950dfd7221289dd59ac31
NickYOOO <outgrowthr@gmail.com>
2022-08-13 18:41:37 +0900

    component > DetailImage 추가

--- src/view/Detail.jsx
+++ src/view/Detail.jsx
@@ -1,9 +1,25 @@
+import styled from "styled-components";
+import Layout from "../components/common/Layout";
+import DetailImage from "../components/DetailImage";
+import DetailContents from "../components/DetailContents"
 const Detail = () => {
     return (
-        <div>
-            나는 디테일
-        </div>
+        <Layout>
+            <Box>
+                <DetailImage />
+                <DetailContents />
+            </Box>
+        </Layout>
     )
 }
 
 export default Detail
+
+const Box = styled.div`
+  display: flex;
+  flex-direction:column;
+  width: 800px ;
+  margin: auto;
+`;
+
+
