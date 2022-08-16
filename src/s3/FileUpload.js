44e9975c9355b3522d93ba60ec52947da4c55e5a
Jisu Lee <jisu2281l@naver.com>
2022-08-16 21:14:39 +0900

    실수...어후...
    어휴....

--- src/s3/FileUpload.js
+++ src/s3/FileUpload.js
@@ -1,8 +1,8 @@
 import AWS from 'aws-sdk';
 
 const OnFileUpload = (e) => {
-    const ACCESS_KEY = "AKIASMFEBMNXERVNX4XG";
-    const SECRET_ACCESS_KEY = "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";
+    const ACCESS_KEY = process.env.ACCESS_KEY;
+    const SECRET_ACCESS_KEY = process.env.SECRET_KEY;
     const REGION = "ap-northeast-2";
     const S3_BUCKET = "mypet-upload-image";
 
