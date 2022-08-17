8fc57b0790bc4627e3a01b7bee16650352b2a511
Jisu Lee <jisu2281l@naver.com>
2022-08-17 22:57:39 +0900

    링크 수정

--- src/redux/modules/posts.js
+++ src/redux/modules/posts.js
@@ -6,7 +6,7 @@ export const getPosts = createAsyncThunk(
   "GET_POSTS",
   async (offset) => {
     try {
-      const {resp} = await axios.get(`?offset=${offset}`);
+      const {resp} = await axios.get(`post?offset=${offset}`);
       return (
         resp.data
       )
