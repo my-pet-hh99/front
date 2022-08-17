a881e1e7aa38d0dc25a8db53eaa4d56ede385b37
SomFist <89966037+somfist@users.noreply.github.com>
2022-08-17 15:22:31 +0900

    mypage 수정제외 완료, 비밀번호 찾기 완료 (#8)

--- src/api/mypageAPI.js
+++ src/api/mypageAPI.js
@@ -1,5 +1,6 @@
 import axios from '../axios/axios'
 
+// 에러쪽 구현 미비
 export const getUserInfo = async () => {
     try {
         const res = await axios.get('/user/me')
@@ -10,21 +11,39 @@ export const getUserInfo = async () => {
 }
 
 export const checkPassword = async (password) => {
+    let answer = {result: null}
     try {
         const res = await axios.get('/user/pwCheck', {
             params: { password }
         })
+        answer.result = res.data.result
+    } catch (err) {
+        answer.result = err.response.data.result
+        answer.message = err.response.data.message
+    }
+    return answer
+}
+
+// 구현 안됨
+export const modifyUserInfo = async (data) => {
+    try {
+        const res = await axios.put('/user/me', data)
         console.log(res)
     } catch (err) {
         console.log(err)
     }
 }
 
-export const modifyUserInfo = async (data) => {
+export const deleteUser = async (password) => {
+    let answer = {result: null}
     try {
-        const res = await axios.put('/user/edit', data)
-        console.log(res)
+        const res = await axios.delete('/user/me', {
+            data: { password }
+        })
+        answer.result = res.data.result
     } catch (err) {
-        console.log(err)
+        answer.result = err.response.data.result
+        answer.message = err.response.data.message
     }
+    return answer
 }
