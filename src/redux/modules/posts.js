1b3167754a22f4f4effe4734ee8e50c45a9bb277
Jisu Lee <jisu2281l@naver.com>
2022-08-17 22:01:55 +0900

    get posts url 변경

--- src/redux/modules/posts.js
+++ src/redux/modules/posts.js
@@ -6,8 +6,12 @@ export const getPosts = createAsyncThunk(
   "GET_POSTS",
   async (offset) => {
     try {
+<<<<<<< Updated upstream
       // const resp = await axios.get(`http://localhost:3001/posts?offset=${offset}`);
       const {resp} = await axios.get(`/post?offset=${offset}`);
+=======
+      const {resp} = await axios.get(`?offset=${offset}`);
+>>>>>>> Stashed changes
       return (
         resp.data
       )
