c91c6dfe0a96a0bede82a50cb3d5a2891bfeed9a
Jisu Lee <108878872+LJS0000@users.noreply.github.com>
2022-08-18 16:02:12 +0900

    최종본
    * mypage 수정제외 완료, 비밀번호 찾기 완료 (#8)
    
    * 링크 수정
    
    * 지완님 합본 (#14)
    
    * component > DetailImage 추가
    
    * components > DetailContents 추가
    
    * 커밋
    
    Co-authored-by: NickYOOO <outgrowthr@gmail.com>
    
    * 무한 스크롤..
    
    * Feature/detail ver1.2 (#16)
    
    * Detail 새로 만드는중...
    
    * DetailContents 작성
    
    * 로그인 최종본 (#17)
    
    * mypage 수정제외 완료, 비밀번호 찾기 완료 (#8) (#9)
    
    * mixed content 에러 해결 1차 시도
    
    * 서버에러 확인용
    
    * 백엔드 연결 2차시도
    
    * 3차 시도
    
    * 4차 시도
    
    * 5차 시도
    
    * modify 작동하게 수정
    
    * 중복메일 오류 핫픽스
    
    Co-authored-by: Jisu Lee <108878872+LJS0000@users.noreply.github.com>
    Co-authored-by: qkrruddjs317@naver.com <솜주먹>
    
    * 오류 수정 작업 중
    
    * 댓글 수정제외 모양새 완료 (#20)
    
    Co-authored-by: qkrruddjs317@naver.com <솜주먹>
    
    * 1차 조절
    
    * 포스트 인풋 수정
    
    * detail 조회 삭제 완료 (#21)
    
    Co-authored-by: qkrruddjs317@naver.com <솜주먹>
    
    * 댓글 작성자만 삭제 가능하게 변경
    
    * 메인 수정
    
    Co-authored-by: SomFist <89966037+somfist@users.noreply.github.com>
    Co-authored-by: NickYOOO <outgrowthr@gmail.com>
    Co-authored-by: Nick <108923582+NickYOOO@users.noreply.github.com>
    Co-authored-by: qkrruddjs317@naver.com <솜주먹>

---
+++ src/components/comment/CommentArea.jsx
@@ -0,0 +1,164 @@
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import { nanoid } from "@reduxjs/toolkit";
+import { useParams } from "react-router-dom";
+
+import useLimitInput from "../../util/useLimitInput";
+
+import Comment from "./Comment";
+import Modal from "../common/Modal";
+import { addComments, readAllComments } from "../../api/commentAPI";
+
+const CommentArea = () => {
+  const [commentsList, setList] = useState([]);
+  const [pagingBtn, setPagingBtn] = useState([]);
+  const [page, setPage] = useState(0);
+
+  const [contents, changeContents, resetContents] = useLimitInput(20);
+
+  const params = useParams().postId;
+  const modalRef = useRef(null);
+
+  const getList = () => {
+    readAllComments(page, params).then((answer) => {
+      setList(answer.list);
+      let pasingNumber = [];
+      for (let i = 0; i < answer.listLength / 3; i++) {
+        pasingNumber.push(i + 1);
+      }
+      setPagingBtn(pasingNumber);
+    });
+  };
+  const modalOpen = () => {
+    modalRef.current.style.opacity = 1;
+    modalRef.current.style.zIndex = 100;
+    modalRef.current.children[0].style.opacity = 1;
+  };
+  const modalClose = () => {
+    modalRef.current.style.opacity = 0;
+    modalRef.current.style.zIndex = -100;
+    modalRef.current.children[0].style.opacity = 0;
+  };
+  const createComments = () => {
+    if (contents === "") {
+      alert("댓글 내용이 없습니다!");
+      return;
+    }
+    addComments({text: contents}, params).then(answer => {
+      if (answer.result) {
+        getList();
+        resetContents();
+      } else {
+        alert(answer.message ? answer.message : '로그인후 이용해 주세요!');
+        resetContents();
+      }
+    });
+  };
+
+  useEffect(() => {
+    getList();
+  }, [page]);
+
+  return (
+    <CmArea>
+      <CommentsTitle className="fcc">
+        <span>댓글</span>
+        <span>
+          <Btn onClick={modalOpen}>등록</Btn>
+        </span>
+      </CommentsTitle>
+      {commentsList.map((c, i) => {
+        return <Comment comments={c} key={i} getList={getList} />;
+      })}
+      <BtnArea className="fcc">
+        {pagingBtn.map((p, i) => {
+          return (
+            <Btn
+              className="fcc"
+              key={p + i}
+              onClick={() => {
+                setPage(p - 1);
+              }}
+            >
+              {p}
+            </Btn>
+          );
+        })}
+      </BtnArea>
+      <Modal ref={modalRef}>
+        <ModalInner>
+          <p>댓글 내용</p>
+          <textarea
+            onChange={changeContents}
+            value={contents}
+            style={{ height: "80px" }}
+            placeholder="20자 제한"
+          ></textarea>
+        </ModalInner>
+        <BtnArea className="fcc">
+          <Btn onClick={createComments}>추가하기</Btn>
+          <Btn onClick={modalClose}>뒤로가기</Btn>
+        </BtnArea>
+      </Modal>
+    </CmArea>
+  );
+};
+
+export default CommentArea;
+
+const CmArea = styled.div`
+  margin-top: 20px;
+  padding: 15px;
+  background-color: #fff;
+`;
+
+const CommentsTitle = styled.div`
+  justify-content: space-between !important;
+  font-size: 20px;
+  font-weight: 600;
+  border-bottom: 1px solid pink;
+  padding-bottom: 10px;
+`;
+
+const BtnArea = styled.div`
+  padding-top: 10px;
+  & * {
+    margin-right: 10px;
+  }
+`;
+const ModalInner = styled.div`
+  margin-bottom: 10px;
+  & * {
+    width: 100%;
+    padding: 10px;
+    font-size: 20px;
+  }
+  & > p {
+    padding: 0;
+    font-size: 25px;
+    color: rgb(255, 204, 204);
+  }
+  & > input,
+  & > textArea {
+    border: 1px solid rgb(255, 204, 204);
+    border-radius: 25px;
+    outline: 0;
+  }
+  & > textArea {
+    resize: none;
+  }
+`;
+
+const Btn = styled.button`
+  cursor: pointer;
+  border: 1px solid rgb(255,204,204);
+  border-radius: 25px;
+  padding: 8px;
+  font-size: 14px;
+  color: white;
+  background: rgb(255,204,204);
+  &:hover {
+    color: rgb(255,204,204);
+    background: white;
+  }
+`
--- src/components/main/ModalPopup.jsx
+++ src/components/main/ModalPopup.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Modal from "react-modal"
 import axios from "../../axios/axios";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import logo from '../../src_assets/logo.png'
 import OnFileUpload from "../../s3/FileUpload";
 
@@ -33,26 +33,34 @@ const ModalPopup = ({isOpen, closeModal}) => {
   };
 
   // post 저장
-  const onSubmitHandler = (post) => {
-    axios.post('/post', post)
-    .then( res => {
-      alert('포스팅 성공')
-      navigate('/')
-    })
-    .catch( error => {console.log(error)})
-  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault()
 
+    if (post.imageUrl==""||post.text==""){
+      alert("내용을 기입해주세요")
+      return
+    }
+    axios.post(`/post`, post)
+    .then( res => {
+        alert('포스팅 성공')
+        window.location.reload()
+    })
+    .catch( error => {if (error.request.status==500)
+      {
+        alert("로그인 이후 이용 가능합니다")
+      }}
+    )
+    closeModal()    
+  };
 
   return(
     <Modal 
         style={customStyles}
-    		isOpen={isOpen}
-			  onRequestClose={closeModal}
+          isOpen={isOpen}
+          onRequestClose={closeModal}
         ariaHideApp={false}
     >
-        <StModalForm
-          onSubmit={()=>{onSubmitHandler(post)}}
-        >
+        <StModalForm>
           <StModalImage>
             {
               change ?
@@ -61,28 +69,31 @@ const ModalPopup = ({isOpen, closeModal}) => {
             }
           </StModalImage>
           <StModalFileUpload
-            required="true"
+            required
             type='file'
             accept="image/jpeg, image/jpg, image/png"
             onChange={(e) => {
               OnFileUpload(e)
               readFile(e.target.files[0])
               setChange(true)
+              setPost({...post, imageUrl:e.target.files[0].name},{});
             }}
           />
           <StModalText
-            required="true"
+            required
             maxLength={150}
             placeholder="내용을 입력하세요. (최대 150자)"
             onChange={(e) => {
               const {value} = e.target;
               setPost({...post, text: value},{});
-              setPost({...post, imageUrl:`https://mypet-upload-image.s3.ap-northeast-2.amazonaws.com/${e.target.files[0].name}`},{});
             }}
           />
           <StModalBtns>
-            <button>저장</button>
-            <button onClick={closeModal}>닫기</button>
+            <button onClick={onSubmitHandler}>저장</button>
+            <button onClick={(e) => {
+              e.preventDefault()
+              window.location.reload()
+            }}>닫기</button>
           </StModalBtns>
         </StModalForm>
     </Modal>
