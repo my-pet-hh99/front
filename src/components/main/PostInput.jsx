f3f7812b494b54260a5c02fb6a15c45b0d7a179f
SomFist <89966037+somfist@users.noreply.github.com>
2022-08-18 10:55:37 +0900

    Dev => main (#18)
    * mypage 수정제외 완료, 비밀번호 찾기 완료 (#8)
    
    * 링크 수정
    
    * 지완님 합본 (#14)
    
    * component > DetailImage 추가
    
    * components > DetailContents 추가
    
    * 커밋
    
    Co-authored-by: NickYOOO <outgrowthr@gmail.com>
    
    * 무한 스크롤..
    
    * Feature/detail ver1.2 (#16)
    
    * Detail 새로 만드는중...
    
    * DetailContents 작성
    
    * 로그인 최종본 (#17)
    
    * mypage 수정제외 완료, 비밀번호 찾기 완료 (#8) (#9)
    
    * mixed content 에러 해결 1차 시도
    
    * 서버에러 확인용
    
    * 백엔드 연결 2차시도
    
    * 3차 시도
    
    * 4차 시도
    
    * 5차 시도
    
    * modify 작동하게 수정
    
    * 중복메일 오류 핫픽스
    
    Co-authored-by: Jisu Lee <108878872+LJS0000@users.noreply.github.com>
    Co-authored-by: qkrruddjs317@naver.com <솜주먹>
    
    Co-authored-by: Jisu Lee <108878872+LJS0000@users.noreply.github.com>
    Co-authored-by: Jisu Lee <jisu2281l@naver.com>
    Co-authored-by: NickYOOO <outgrowthr@gmail.com>
    Co-authored-by: Nick <108923582+NickYOOO@users.noreply.github.com>

--- src/components/main/PostInput.jsx
+++ src/components/main/PostInput.jsx
@@ -20,13 +20,8 @@ const PostInput = () => {
     alert("로그인 이후 이용하실 수 있습니다")
   }
 
-  // 로그인 확인
-  // const getUser = useSelector(state => state.user.loginUser)
-  // const [user, setUser] = useState(getUser)
-
   return(
     <>
-      {/* <StPostModalBtn onClick={ 로그인 정보 확인 ? loginAlert : openModal }> */}
       <StPostModalBtn onClick={openModal}>
         <p>내 새끼 자랑하는 글을 써보세요! 🐶🐹🐱🐰</p>
       </StPostModalBtn>
