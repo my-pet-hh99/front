d997b5a08d02d7b291a182aa6e50d06c25e1625b
SomFist <89966037+somfist@users.noreply.github.com>
2022-08-18 12:28:57 +0900

    댓글 수정제외 모양새 완료 (#20)
    Co-authored-by: qkrruddjs317@naver.com <솜주먹>

---
+++ src/api/commentAPI.js
@@ -0,0 +1,38 @@
+import axios from "../axios/axios";
+
+export const readAllComments = async (page, postId) => {
+    let answer = {result: null}
+    try {
+        const res = await axios.get(`/comment/${postId}`);
+        answer.result = res.data.result
+        answer.list = res.data.data.slice(page * 3, page * 3 + 3)
+        console.log(answer.list)
+        answer.listLength = res.data.data.length
+    } catch(err) {
+        // console.log(err)
+    }
+  return answer;
+};
+
+export const addComments = async (newComments, postId) => {
+    let answer = {result: null}
+    try {
+        const res = await axios.post(`/comment/${postId}`, newComments);
+        answer.result = res.data.result
+    } catch(err) {
+        answer.result = err.response.data.result
+        answer.message = err.response.data.message
+    }
+    return answer
+};
+
+export const deleteComments = async (commentId) => {
+    let answer = {result: null}
+    try {
+        const res = await axios.delete(`/comment/${commentId}`);
+        answer.result = res.data.result
+    } catch(err) {
+        // console.log(err)
+    }
+    return answer
+};
--- src/view/Detail.jsx
+++ src/view/Detail.jsx
@@ -2,12 +2,14 @@ import styled from "styled-components";
 import Layout from "../components/common/Layout";
 import Header from "../components/Header"
 import DetailContents from "../components/detail/DetailContents"
+import CommentArea from "../components/comment/CommentArea";
 const Detail = () => {
     return (
         <Layout>
             <Box>
                 <Header/>
                 <DetailContents />
+                <CommentArea/>
             </Box>
         </Layout>
     )
